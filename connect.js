b8e9fa79d617b696161c2296bfa2c60742f48f00
Jonathan Coppens <jonathan.b.coppens@gmail.com>
2023-08-31 23:00:39 +0200

    changement 'connect.js'

--- connect.js
+++ connect.js
@@ -3,24 +3,23 @@ function welcomeMessage() {
     let surname = document.getElementById("surname").value;
     /* let welcome = alert(`Bienvenue à toi, ${surname} !`); */
 
-    if(surname == "") {
-        alert("Veuillez entre votre nom");
+        // vérifie que le nom entré n'est pas vide et ne contient pas de caractères spéciaux
+    if(surname == "" && (/[^a-zA-Z0-9-]/g)) {
+        alert("Veuillez entrer votre nom");
     } else {
-        alert(`Bienvenue sur le site ${surname} !`)
+        alert(`Bienvenue sur le site ${surname} !`);
+        // récupération de l'icone
+        let userIcon = document.querySelector(".user-icon");
+        
+        // récupération du nom de l'utilisateur connecté
+        let userName = document.createElement("span");
+        userName.classList.add("user-name");
+        userName.textContent = surname.value;
+
+        
+        // ajout de l'élément 'span' à l'élément 'userIcon'
+        userIcon.appendChild(userName);
     }
 }
 // soumet le formulaire et y intègre la fonction précédente
 document.querySelector("form").addEventListener("submit", welcomeMessage);
-
-
-
-/* function alert(message) {
-    const alertBox = document.createElement("div");
-    alertBox.classList.add("alert");
-    alertBox.textContent = message;
-  
-    document.body.appendChild(alertBox);
-  
-    // Attendre que l'utilisateur ferme la fenêtre d'alerte
-    alertBox.addEventListener("click", () => alertBox.remove());
-  } */
